b0f1a173530190e66723e7f20455b2fee44b1cea
EmiValdeCo <20190010@ricaldone.edu.sv>
2024-05-16 14:43:06 -0600

    Creación de las carpetas y pantallas para la navegacion

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+/* Importación de las pantallas */
+import Pantalla1 from './src/screens/Pantalla1';
+import Pantalla2 from './src/screens/Pantalla2';
+
+const Tab = createBottomTabNavigator();
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Pantalla1" component={Pantalla1} />
+        <Tab.Screen name="Pantalla2" component={Pantalla2} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
