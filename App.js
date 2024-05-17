2c365318be84b1939effd2c9c517d8bf44abd6f8
EmiValdeCo <20190010@ricaldone.edu.sv>
2024-05-16 21:51:05 -0600

    Array de Comida y imagenes de comida

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, Image, Animated } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { StatusBar } from 'expo-status-bar';
+import { Ionicons } from '@expo/vector-icons';
 
 /* Importación de las pantallas */
 import Pantalla1 from './src/screen/Pantalla1';
@@ -10,25 +12,52 @@ import Pantalla2 from './src/screen/Pantalla2';
 const Tab = createBottomTabNavigator();
 
 export default function App() {
+
   return (
     <NavigationContainer>
       <StatusBar style="auto" />
       <Tab.Navigator
-        initialRouteName="Pantalla1"  // Especifica la pantalla inicial aquí
-        screenOptions={{
+        initialRouteName="Pantalla1"
+        screenOptions={({ route }) => ({
           headerShown: false,
-        }}>
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Pantalla1') {
+              iconName = focused ? 'home' : 'home-outline';
+            } else if (route.name === 'Pantalla2') {
+              iconName = focused ? 'settings' : 'settings-outline';
+            }
+            
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: '#2c3e50',
+          tabBarInactiveTintColor: 'gray',
+          tabBarStyle: {
+            backgroundColor: '#f5f5f5', // Color gris clarito
+            borderTopWidth: 0,
+            elevation: 10,
+            height: 60, // altura 
+            paddingBottom: 10, //  padding 
+          },
+          tabBarLabelStyle: {
+            fontSize: 12,
+            marginBottom: 5, //  espacio inferior del label
+          },
+        })}
+      >
         <Tab.Screen 
           name="Pantalla1" 
           component={Pantalla1} 
-          options={{ tabBarLabel: 'Pantalla 1' }}
+          options={{ tabBarLabel: 'Inicio' }}
         />
         <Tab.Screen 
           name="Pantalla2" 
           component={Pantalla2} 
-          options={{ tabBarLabel: 'Pantalla 2' }}
+          options={{ tabBarLabel: 'Ajustes' }}
         />
       </Tab.Navigator>
     </NavigationContainer>
   );
 }
+
---
+++ src/data/comida.js
@@ -0,0 +1,40 @@
+const Comida = [
+    {
+        id: '1',
+        title: 'Pupusas',
+        description: 'Las pupusas son una comida tradicional salvadoreña, hecha de masa de maíz o arroz rellena de queso, chicharrón, frijoles, o una combinación de estos.',
+        src: require('../img/pupusas.jpg'),
+    },
+    {
+        id: '2',
+        title: 'Atol de Elote',
+        description: 'El atol de elote es una bebida tradicional de maíz dulce, preparada con maíz tierno, leche, azúcar y canela. Es popular en varios países de Centroamérica.',
+        src: require('../img/atol.jpg'),
+    },
+    {
+        id: '3',
+        title: 'Sopa de pata',
+        description: 'La sopa de pata es un plato típico de El Salvador, hecho con patas de res, verduras, y especias, que se sirve caliente y es conocido por su sabor y propiedades nutritivas.',
+        src: require('../img/sopa_de_pata.jpg'),
+    },
+    {
+        id: '4',
+        title: 'Tamales de Elote',
+        description: 'Los tamales de elote son una delicia tradicional hechos de una masa de maíz dulce envuelta en hojas de maíz y cocidos al vapor. Son suaves, dulces y a menudo se disfrutan como postre o merienda.',
+        src: require('../img/tamales.jpg'),
+    },
+    {
+        id: '5',
+        title: 'Yuca Frita con Chicharrón',
+        description: 'La yuca frita con chicharrón es un plato típico salvadoreño que consiste en yuca frita acompañada de chicharrón, curtido y salsa de tomate.',
+        src: require('../img/yuca.jpg'),
+    },
+    {
+        id: '6',
+        title: 'Empanadas de Leche',
+        description: 'Las empanadas de leche son un postre salvadoreño hecho de masa de plátano rellena con un dulce de leche, luego fritas hasta dorarse. Son crujientes por fuera y suaves por dentro.',
+        src: require('../img/empanada.jpg'),
+    }
+];
+
+export default Comida;
